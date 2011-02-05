2c1853576f9001c7d27157aaa3b7672f15889dc5
Lady Josephine <Rob Ashton@ws-jotorylaptop.(none)>
2011-02-05 21:39:32 +0000

    adding matrix class and multiplying matrix by scalar

--- jorender/JoRender.Tests/Matrix4Tests.cs
+++ jorender/JoRender.Tests/Matrix4Tests.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using JoRender;
 
 namespace JoRender.Tests
 {
-    class Matrix4Tests
+    [TestFixture]
+    public class Matrix4Tests
     {
+        // Multiply a matrix by scalar
+
+        [Test]
+        public void When_Multiplying_A_Matrix_By_A_Scalar_Matrix_With_Result_Is_Returned()
+        {
+            //Arrange
+            Matrix4 matrixOne = new Matrix4(1.0, 2.0, 3.0, 4.0, 2.0, 3.0, 4.0, 5.0, 3.0, 4.0, 5.0, 6.0, 4.0, 5.0, 6.0, 7.0);
+            double scalarOne = 2.0;
+
+            //Act
+            Matrix4 result = matrixOne.Multiply(scalarOne);
+
+            //Assert
+            Assert.AreEqual(2.0, result.XX);
+            Assert.AreEqual(4.0, result.XY);
+            Assert.AreEqual(6.0, result.XZ);
+            Assert.AreEqual(8.0, result.XW);
+            Assert.AreEqual(4.0, result.YX);
+            Assert.AreEqual(6.0, result.YY);
+            Assert.AreEqual(8.0, result.YZ);
+            Assert.AreEqual(10.0, result.YW);
+            Assert.AreEqual(6.0, result.ZX);
+            Assert.AreEqual(8.0, result.ZY);
+            Assert.AreEqual(10.0, result.ZZ);
+            Assert.AreEqual(12.0, result.ZW);
+            Assert.AreEqual(8.0, result.WX);
+            Assert.AreEqual(10.0, result.WY);
+            Assert.AreEqual(12.0, result.WZ);
+            Assert.AreEqual(14.0, result.WW);
+        }
+
+
+
+
         // Multiply two matrices
 
         // Create origin matrix
@@ -15,6 +52,6 @@ namespace JoRender.Tests
 
         // Calculate other crazy value shit
 
-        // Multiply by scalar
+  
     }
 }
--- jorender/JoRender/Matrix4.cs
+++ jorender/JoRender/Matrix4.cs
@@ -5,7 +5,75 @@ using System.Text;
 
 namespace JoRender
 {
-    class Matrix4
+    public class Matrix4
     {
+        double xx;
+        double xy;
+        double xz;
+        double xw;
+
+        double yx;
+        double yy;
+        double yz;
+        double yw;
+
+        double zx;
+        double zy;
+        double zz;
+        double zw;
+
+        double wx;
+        double wy;
+        double wz;
+        double ww;
+
+        public double XX { get { return xx; } }
+        public double XY { get { return xy; } }
+        public double XZ { get { return xz; } }
+        public double XW { get { return xw; } }
+
+        public double YX { get { return yx; } }
+        public double YY { get { return yy; } }
+        public double YZ { get { return yz; } }
+        public double YW { get { return yw; } }
+
+        public double ZX { get { return zx; } }
+        public double ZY { get { return zy; } }
+        public double ZZ { get { return zz; } }
+        public double ZW { get { return zw; } }
+
+        public double WX { get { return wx; } }
+        public double WY { get { return wy; } }
+        public double WZ { get { return wz; } }
+        public double WW { get { return ww; } }
+
+        public Matrix4(double xx, double xy, double xz, double xw, double yx, double yy, double yz, double yw, double zx, double zy, double zz, double zw, double wx, double wy, double wz, double ww)
+        {
+            this.xx = xx;
+            this.xy = xy;
+            this.xz = xz;
+            this.xw = xw;
+
+            this.yx = yx;
+            this.yy = yy;
+            this.yz = yz;
+            this.yw = yw;
+
+            this.zx = zx;
+            this.zy = zy;
+            this.zz = zz;
+            this.zw = zw;
+
+            this.wx = wx;
+            this.wy = wy;
+            this.wz = wz;
+            this.ww = ww;
+        }
+
+        public Matrix4 Multiply(double Scalar)
+        {
+            return new Matrix4(xx * Scalar, xy * Scalar, xz * Scalar, xw * Scalar, yx * Scalar, yy * Scalar, yz * Scalar, yw * Scalar, zx * Scalar, zy * Scalar, zz * Scalar, zw * Scalar, wx * Scalar, wy * Scalar, wz * Scalar, ww * Scalar);
+        }
+        
     }
 }
